5f913bf21fa71aafb4a3eca8418222cbee1f0a0b
AdityaSharma <aditya.sharma@elitessafety.com>
2020-09-13 18:30:23 +0530

    Update DB design for roles and platforms

---
+++ CCMS.Database/000_CleanUp.sql
@@ -0,0 +1,33 @@
+DROP PACKAGE pkg_auth;
+
+DROP PACKAGE pkg_app_users;
+
+DROP PROCEDURE p_audit_log;
+
+DROP TABLE case_proceedings;
+
+DROP TABLE case_actors;
+
+DROP TABLE case_dates;
+
+DROP TABLE court_cases;
+
+DROP TABLE attachments;
+
+DROP TABLE actor_types;
+
+DROP TABLE proceeding_decisions;
+
+DROP TABLE locations;
+
+DROP TABLE case_types;
+
+DROP TABLE courts;
+
+DROP TABLE lawyers;
+
+DROP TABLE user_sessions;
+
+DROP TABLE audit_log;
+
+DROP TABLE app_users;
