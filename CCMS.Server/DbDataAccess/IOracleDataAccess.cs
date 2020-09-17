8b045043fed045043e2f5390f6638044e9906c8d
AdityaSharma <aditya.sharma@elitessafety.com>
2020-09-17 13:47:54 +0530

    Init CCMS.Server project

---
+++ CCMS.Server/DbDataAccess/IOracleDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace CCMS.Server.DbDataAccess
+{
+    public interface IOracleDataAccess
+    {
+        Task<int> ExecuteAsync(string sql, object parameters = null, CommandType commandType = CommandType.StoredProcedure);
+        Task<IEnumerable<T>> QueryAsync<T>(string sql, object parameters = null, CommandType commandType = CommandType.StoredProcedure);
+        Task<T> QueryFirstOrDefaultAsync<T>(string sql, object parameters = null, CommandType commandType = CommandType.StoredProcedure);
+    }
+}
---
+++ CCMS.Server/DbDataAccess/OracleDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace CCMS.Server.DbDataAccess
+{
+    public class OracleDataAccess : IOracleDataAccess
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public OracleDataAccess(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public Task<T> QueryFirstOrDefaultAsync<T>(string sql, object parameters = null, CommandType commandType = CommandType.StoredProcedure)
+        {
+            DefaultTypeMap.MatchNamesWithUnderscores = true;
+            return _dbConnection.QueryFirstOrDefaultAsync<T>(sql, parameters, commandType: commandType);
+        }
+
+        public Task<IEnumerable<T>> QueryAsync<T>(string sql, object parameters = null, CommandType commandType = CommandType.StoredProcedure)
+        {
+            DefaultTypeMap.MatchNamesWithUnderscores = true;
+            return _dbConnection.QueryAsync<T>(sql, parameters, commandType: commandType);
+        }
+
+        public Task<int> ExecuteAsync(string sql, object parameters = null, CommandType commandType = CommandType.StoredProcedure)
+        {
+            return _dbConnection.ExecuteAsync(sql, parameters, commandType: commandType);
+        }
+    }
+}
