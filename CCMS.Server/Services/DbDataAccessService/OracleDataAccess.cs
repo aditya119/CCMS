d94e81595253789f5dd1d3904552c1e1df3ccd08
AdityaSharma <aditya.sharma@elitessafety.com>
2020-12-24 16:06:38 +0530

    Remove unused import

--- CCMS.Server/Services/DbDataAccessService/OracleDataAccess.cs
+++ CCMS.Server/Services/DbDataAccessService/OracleDataAccess.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Threading.Tasks;
-using System.Transactions;
 using CCMS.Server.Models;
 
 namespace CCMS.Server.Services.DbDataAccessService
