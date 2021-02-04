4eddcaec81b62d2b4a2076ca7c840bd65cd1a510
Aditya Sharma <aditya.sharma@elitessafety.com>
2021-02-04 22:14:27 +0530

    Update PendingProceedings.razor

--- CCMS.Server/Utilities/AuthenticateSession.cs
+++ CCMS.Server/Utilities/AuthenticateSession.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
 
 namespace CCMS.Server.Utilities
 {
